b3fcbaa2e38949156f33744090cf045a94686efc
Ansari Mohammed Ahmed <ansari.mohammed2930@gmail.com>
2025-08-15 22:15:34 +0530

    app router

---
+++ lib/components/app_router.dart
@@ -0,0 +1,50 @@
+import 'dart:convert' show jsonDecode;
+
+import 'package:fitvault/views/bmi_calculator/bmi_calculator_view.dart';
+import 'package:fitvault/views/bmi_result/bmi_result_view.dart';
+import 'package:go_router/go_router.dart' show GoRouter, GoRoute;
+
+final GoRouter approute = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      name: 'home',
+      path: '/',
+      builder: (context, state) => const BMICalculatorView(),
+      routes: [
+        GoRoute(
+          name: 'result',
+          path: 'result',
+          builder: (context, state) {
+            final params = state.uri.queryParameters;
+            final bmiResult = params['bmiResult'] ?? '0.0 ';
+            final gender = params['gender'] ?? 'Male';
+            final weightInKg =
+                double.tryParse(params['weightInKg'] ?? '0.0') ?? 0.0;
+            final heightInFeet = params['heightInFeet'] ?? "0' 0\"";
+
+            final idealWeightRange = params['idealWeightRange'];
+            final Map<String, double> idealWeightRangeMap =
+                idealWeightRange != null
+                ? Map<String, double>.from(
+                    jsonDecode(idealWeightRange).map(
+                      (k, v) => MapEntry(
+                        k,
+                        (v as num).toDouble(),
+                      ),
+                    ),
+                  )
+                : {};
+            return BmiResultView(
+              bmiResult: bmiResult,
+              weightInKg: weightInKg,
+              heightInFeet: heightInFeet,
+              gender: gender.toString(),
+              idealWeightRange: idealWeightRangeMap,
+            );
+          },
+        ),
+      ],
+    ),
+  ],
+);
