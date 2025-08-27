7303f0faf77dca29f195400f34862a0cecec3e69
Ansari Mohammed Ahmed <ansari.mohammed2930@gmail.com>
2025-08-27 20:09:13 +0530

    app router updated with locker home

--- lib/components/app_router.dart
+++ lib/components/app_router.dart
@@ -2,6 +2,7 @@ import 'dart:convert' show jsonDecode;
 
 import 'package:fitvault/views/bmi_calculator/bmi_calculator_view.dart';
 import 'package:fitvault/views/bmi_result/bmi_result_view.dart';
+import 'package:fitvault/views/locker_home/locker_home_view.dart';
 import 'package:fitvault/views/login/login_view.dart';
 import 'package:fitvault/views/register/register_view.dart';
 import 'package:go_router/go_router.dart';
@@ -53,6 +54,10 @@ final GoRouter approute = GoRouter(
         GoRoute(
           path: '/register',
           builder: (context, state) => const RegisterView(),
+        ),
+        GoRoute(
+          path: '/lockerhome',
+          builder: (context, state) => LockerHomeView(),
         ),
       ],
     ),
